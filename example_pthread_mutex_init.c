83bc03b6bf2fb396749b884a3bf700e206424795
Igor Menezes <igorsilvamenezes.gmail.com>
2023-01-27 21:36:48 -0300

    Exemplo de utilização da função pthread_mutex_init - Alteração

--- example_pthread_mutex_init.c
+++ example_pthread_mutex_init.c
@@ -1,40 +1,22 @@
 #include <pthread.h>
 #include <stdio.h>
 
-pthread_mutex_t mutex;
-int shared_resource = 0;
-
-void *thread_func(void *arg)
-{
-    for(int i = 0; i < 1000000; i++){
-        pthread_mutex_lock(&mutex);
-        shared_resource++;
-        pthread_mutex_unlock(&mutex);
-    }
-    return NULL;
-}
+//Declare a mutex
+pthread_mutex_t my_mutex;
 
 int main()
 {
-    pthread_mutex_init(&mutex, NULL);
-    pthread_t thread1, thread2;
+    int ret;
 
-    int status = pthread_create(&thread1, NULL, thread_func, NULL);
-    if(status != 0){
-        printf("Error creating thread 1\n");
-        return 1;
+    //Initialize the mutex
+    ret = pthread_mutex_init(&my_mutex, NULL);
+    if(ret != 0){
+        printf("Error initializing mutex\n");
+        return -1;
     }
 
-    status = pthread_create(&thread2, NULL, thread_func, NULL);
-    if(status != 0){
-        printf("Error creating thread 2\n");
-        return 1;
-    }
+    //Destroy the mutrex
+    pthread_mutex_destroy(&my_mutex);
 
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-
-    printf("Valor final do recurso compartilhada: %d\n", shared_resource);
-    pthread_mutex_destroy(&mutex);
     return 0;
 }
